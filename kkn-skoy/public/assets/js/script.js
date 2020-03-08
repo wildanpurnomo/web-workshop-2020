f9508de11717979d653ebbe13488e74c12a4c66c
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2020-03-09 06:48:29 +0700

    kkn

--- kkn-skoy/public/assets/js/script.js
+++ kkn-skoy/public/assets/js/script.js
@@ -1,3 +1,4 @@
+// JQuery Approach
 $(document).ready(() => {
     $('#form-kkn').on('submit', () => {
         var inputName = $('#input-name').val();
@@ -12,7 +13,7 @@ $(document).ready(() => {
             data: inputData,
             success: (response) => {
                 // do something with response
-                
+
                 location.reload();
             }
         });
@@ -21,7 +22,18 @@ $(document).ready(() => {
     });
 
 
-    $('.btn-danger').on('click', () => {
+    $('.btn-danger').on('click', function () {
         // handle delete
+        var name = $(this).attr('id');
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/delete/' + name,
+            success: (response) => {
+                // do something with response
+
+                location.reload();
+            }
+        });
     });
-})
+});
